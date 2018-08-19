7869d2789f56a252b393c2a924e6dcb3ff118983
Nadim <baraja.nadim63@gmail.com>
2018-08-19 22:43:16 +0700

    membuat kolom untuk table categories

--- database/migrations/2018_08_19_153902_create_table_categories.php
+++ database/migrations/2018_08_19_153902_create_table_categories.php
@@ -15,6 +15,7 @@ class CreateTableCategories extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nama');
             $table->timestamps();
         });
     }
